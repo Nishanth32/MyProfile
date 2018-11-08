cd3086c467fe4dcb3be6520676b946b6f6381742
Nishanth <jebestinishanth@hotmail.com>
2018-11-08 17:53:42 -0500

    Last Changes

--- MyProfile/Startup.cs
+++ MyProfile/Startup.cs
@@ -38,6 +38,7 @@ namespace MyProfile
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseDeveloperExceptionPage();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
